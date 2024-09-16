c5fa4acaf07ca9813d80d1ea05b07c858f6e65d2
Hanhtunaung97 <littlehanhtunaung97>
2024-09-16 21:32:17 +0800

    folder setup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), jsonServer({ delay: 1000, dir: "./mock", port: 8000 })],
+});
