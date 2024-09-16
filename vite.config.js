61223ffd63000602cd004a1780393c352d9767b1
Hanhtunaung97 <littlehanhtunaung97>
2024-09-16 22:20:34 +0800

    json server add

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), jsonServer({ delay: 1000, dir: "./mock", port: 8000 })],
+  plugins: [react()],
 });
