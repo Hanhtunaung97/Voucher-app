c7073c88dedb97a156f081f1520b036f12c726b5
Hanhtunaung97 <littlehanhtunaung97>
2024-09-26 21:54:11 +0800

    nav feature add

--- src/components/nav/HeaderComponent.jsx
+++ src/components/nav/HeaderComponent.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import ContainerComponent from "../utilities/ContainerComponent";
+import { Link } from "react-router-dom";
 
 const HeaderComponent = () => {
   return (
     <header className="mb-5">
       <ContainerComponent>
         <div className="flex flex-col gap-1 py-5">
-          <h1 className="font-heading font-bold text-lg md:text-2xl text-slate-800">
+          <Link to={"/"} className="font-heading font-bold text-lg md:text-2xl text-slate-800">
             Voucher Management
-          </h1>
+          </Link>
           <h5 className="font-medium text-slate-500 text-sm md:text-base">
             MMS Software
           </h5>
